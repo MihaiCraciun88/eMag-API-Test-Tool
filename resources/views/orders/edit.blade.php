74d057c953a0981a1e9fd5ce449df25f3019e6a3
MihaiCraciun88 <31093928+MihaiCraciun88@users.noreply.github.com>
2024-01-06 11:52:08 +0200

    Updates for order status

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -20,11 +20,30 @@
     @endif
 
     <div class="row">
-        <div class="col-12 mb-3">
-            <div class="mb-3"><strong>Customer name:</strong> {{ $order->customer->name }}</div>
-            <div class="mb-3"><strong>Customer email:</strong> {{ $order->customer->email }}</div>
-            <div class="mb-3"><strong>Customer phone:</strong> {{ $order->customer->billing_phone }}</div>
-        </div>
+
+        <form action="{{ route('orders.update', $order) }}" method="post">
+            @csrf
+            @method('PATCH')
+            <div class="col-12 mb-3">
+                <div class="mb-3"><strong>Customer name:</strong> {{ $order->customer->name }}</div>
+                <div class="mb-3"><strong>Customer email:</strong> {{ $order->customer->email }}</div>
+                <div class="mb-3"><strong>Customer phone:</strong> {{ $order->customer->billing_phone }}</div>
+                <div class="mb-3">
+                    <strong>Status:</strong>
+                    <select name="status" class="form-select form-select-sm">
+                        <option value="{{ App\Models\Order::STATUS_CANCELLED }}"{{ $order->status == App\Models\Order::STATUS_CANCELLED ? ' selected' : '' }}>CANCELLED</option>
+                        <option value="{{ App\Models\Order::STATUS_NEW }}"{{ $order->status == App\Models\Order::STATUS_NEW ? ' selected' : '' }}>NEW</option>
+                        <option value="{{ App\Models\Order::STATUS_IN_PROGRESS }}"{{ $order->status == App\Models\Order::STATUS_IN_PROGRESS ? ' selected' : '' }}>IN_PROGRESS</option>
+                        <option value="{{ App\Models\Order::STATUS_PREPARED }}"{{ $order->status == App\Models\Order::STATUS_PREPARED ? ' selected' : '' }}>PREPARED</option>
+                        <option value="{{ App\Models\Order::STATUS_FINALIZED }}"{{ $order->status == App\Models\Order::STATUS_FINALIZED ? ' selected' : '' }}>FINALIZED</option>
+                        <option value="{{ App\Models\Order::STATUS_RETURNED }}"{{ $order->status == App\Models\Order::STATUS_RETURNED ? ' selected' : '' }}>RETURNED</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-sm btn-primary" type="submit">Submit</button>
+                </div>
+            </div>
+        </form>
         <div class="col-12 mb-3">
             <h2>Produse</h2>
             <table class="table">
@@ -84,5 +103,4 @@
             </table>
         </div>
     </div>
-    
 @endsection
